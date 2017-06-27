150713346b7f8460b950efc9339c24c418745877
willin <willin@willin.org>
2017-06-27 16:02:36 +0800

    fix lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,19 @@
 module.exports = {
+  root: true,
   extends: [
     'dwing',
     'plugin:vue/recommended'
   ],
   rules: {
+    'no-new': 0,
+    'import/extensions': ['error', 'always', { 'js': 'never', 'vue': 'never' }],
     'import/no-extraneous-dependencies': 0
+  },
+  settings: {
+    'import/resolver': {
+      webpack: {
+        config: 'config/base.js'
+      }
+    }
   }
 };
